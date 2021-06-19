f842b41147e13427120cba8d684e6182ce532dd9
Michaek Asama Luk <lunium1000@gmail.com>
2021-06-19 21:46:36 +0800

    stash

--- gen_video.py
+++ gen_video.py
@@ -7,8 +7,8 @@ from easydict import EasyDict
 import cv2
 
 args = EasyDict({
-    'dataset': './data/3x3x3/apple',
-    'savedir': './results/3x3x3/apple',
+    'dataset': './data/t5',
+    'savedir': './results/t5',
     'type': ['light', 'time', 'view'],
     'dims': [3, 3, 3],
     'DSfactor': 8,
@@ -42,78 +42,132 @@ model.eval()
 
 def gen_video():
 
-    precomputed_flows = []   # 把所有图片的flow图提前计算出来，后面可以直接用
+    precomputed_flows = []
 
     for i in range(len(coordinates)):
         flows_out = model(torch.from_numpy(coordinates[[i],::]).permute(0,3,1,2).float().cuda(), flow=True)
         precomputed_flows.append(flows_out[0,::].cpu().detach().numpy())
-
+      
     precomputed_flows = np.stack(precomputed_flows,0)
 
     print(precomputed_flows.shape)
 
     print('<Gen Video> Number of neighbors for interpolation: {}'.format(num_n))
 
-    max_light = dims[0]-1
-    max_view = dims[1]-1
-    max_time = dims[2]-1
+    if(args.type == ['light','view','time']):
+    
+        max_light = dims[0]-1
+        max_view = dims[1]-1
+        max_time = dims[2]-1
 
-    X_light = np.linspace(0, max_light, max_light * scale)
-    X_light = np.append(X_light, np.flip(X_light))
-    X_view = np.linspace(0, max_view, max_view * scale)
-    X_view = np.append(X_view, np.flip(X_view))
-    X_time = np.linspace(0, max_time, max_time * scale)
-    X_time = np.append(X_time, np.flip(X_time))
-    X_light_center = max_light * 0.5 * np.ones(X_light.shape)
-    X_view_center = max_view * 0.5 * np.ones(X_view.shape)
-    X_time_center = max_time * 0.5 * np.ones(X_time.shape)
+        X_light = np.linspace(0, max_light, max_light * scale)
+        X_light = np.append(X_light, np.flip(X_light))
+        X_view = np.linspace(0, max_view, max_view * scale)
+        X_view = np.append(X_view, np.flip(X_view))
+        X_time = np.linspace(0, max_time, max_time * scale)
+        X_time = np.append(X_time, np.flip(X_time))
+        X_light_center = max_light * 0.5 * np.ones(X_light.shape)
+        X_view_center = max_view * 0.5 * np.ones(X_view.shape)
+        X_time_center = max_time * 0.5 * np.ones(X_time.shape)
 
-    all_dimensions = {
-        'light': np.stack([X_light, X_view_center, X_time_center], 1),
-        'view': np.stack([X_light_center, X_view, X_time_center], 1),
-        'time': np.stack([X_light_center, X_view_center, X_time], 1),
-        'light_view': np.stack([X_light, X_view, X_time_center], 1),
-        'light_time': np.stack([X_light, X_view_center, X_time], 1),
-        'view_time': np.stack([X_light_center, X_view, X_time], 1),
-        'light_view_time': np.stack([X_light, X_view, X_time], 1)
-    }
+        all_dimensions = {
+            'light': np.stack([X_light, X_view_center, X_time_center], 1),
+            'view': np.stack([X_light_center, X_view, X_time_center], 1),
+            'time': np.stack([X_light_center, X_view_center, X_time], 1),
+            'light_view': np.stack([X_light, X_view, X_time_center], 1),
+            'light_time': np.stack([X_light, X_view_center, X_time], 1),
+            'view_time': np.stack([X_light_center, X_view, X_time], 1),
+            'light_view_time': np.stack([X_light, X_view, X_time], 1)
+        }
 
-    for case, idx in all_dimensions.items():
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        out = cv2.VideoWriter('{}/rendered videos/rendered_{}.mp4'.format(args.savedir, case), fourcc, fps, (img_w, img_h))
+        for case, idx in all_dimensions.items():
 
-        print('--------- interpolating {} ---------'.format(case))
+            fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+            out = cv2.VideoWriter('{}/rendered videos/rendered_{}.mp4'.format(args.savedir, case), fourcc, fps, (img_w, img_h))
 
-        for i in range(len(idx)):
+            print('--------- interpolating {} ---------'.format(case))
 
-            input_coord = np.array([[[idx[i,:]]]])
-            print("input_coord",input_coord)
-            # find the num_n coords that are cloest to the input_coord
-            indices = np.argsort(np.sum(np.square(input_coord[0,0,0,:]-coordinates[:,0,0,:]), -1))[:num_n]  #寻找平方距离
-            print("indices", indices)
-            neighbor_coord = coordinates[indices,::]
-            print("neighbor_coord", neighbor_coord)
-            input_neighbors = img_data[indices,::]
-            input_flows = precomputed_flows[indices,::]
+            for i in range(len(idx)):
 
-            time_index = indices // (dims[0]*dims[1])
-            rest = indices % (dims[0]*dims[1])
-            view_index = rest % dims[1]
-            albedo_index = view_index*dims[1] + time_index
-            img = model(
-                torch.from_numpy(input_coord).cuda().float().permute(0,3,1,2), 
-                neighbors=torch.from_numpy(input_neighbors).cuda().float().permute(0,3,1,2),
-                albedo_index=torch.from_numpy(albedo_index).cuda().long(),
-                coord_neighbor=torch.from_numpy(neighbor_coord).cuda().permute(0,3,1,2), 
-                neighbors_flow=torch.from_numpy(input_flows).cuda(), 
-                test=True
-            )
-            img = img.cpu().detach()[0,::].permute(1,2,0).numpy()
-            img = np.minimum(np.maximum(img, 0.0), 1.0)
-            out.write(np.uint8(img * 255))
-            print('\r<Gen Video> interpolated image {} of {}'.format(i+1, len(idx)), end=' ')
+                input_coord = np.array([[[idx[i,:]]]])
+                # find the num_n coords that are cloest to the input_coord
+                indices = np.argsort(np.sum(np.square(input_coord[0,0,0,:]-coordinates[:,0,0,:]), -1))[:num_n]
 
-        out.release()
-        print('')
+                neighbor_coord = coordinates[indices,::]
+
+                input_neighbors = img_data[indices,::]
+                input_flows = precomputed_flows[indices,::]
+
+                time_index = indices // (dims[0]*dims[1])
+                rest = indices % (dims[0]*dims[1])
+                view_index = rest % dims[1]
+                albedo_index = view_index*dims[1] + time_index
+
+                img = model(
+                    torch.from_numpy(input_coord).cuda().float().permute(0,3,1,2), 
+                    neighbors=torch.from_numpy(input_neighbors).cuda().float().permute(0,3,1,2),
+                    albedo_index=torch.from_numpy(albedo_index).cuda().long(),
+                    coord_neighbor=torch.from_numpy(neighbor_coord).cuda().permute(0,3,1,2), 
+                    neighbors_flow=torch.from_numpy(input_flows).cuda(), 
+                    test=True
+                )
+
+                img = img.cpu().detach()[0,::].permute(1,2,0).numpy()
+                img = np.minimum(np.maximum(img, 0.0), 1.0)
+                out.write(np.uint8(img * 255))
+                print('\r<Gen Video> interpolated image {} of {}'.format(i+1, len(idx)), end=' ')
+
+            out.release()
+            print('')
+        
+        else:
+            max_coord = dims[0]-1
+
+            X_coord = np.linspace(0, max_coord, max_coord * scale)
+
+            all_dimensions = {
+                args.type[0]: np.stack([X_coord], 1),
+            }
+
+            for case, idx in all_dimensions.items():
+
+                fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+                out = cv2.VideoWriter('{}/rendered videos/rendered_{}.mp4'.format(args.savedir, case), fourcc, fps, (img_w, img_h))
+
+                print('--------- interpolating {} ---------'.format(case))
+
+                for i in range(len(idx)):
+
+                    input_coord = np.array([[[idx[i,:]]]])
+                    # find the num_n coords that are cloest to the input_coord
+                    indices = np.argsort(np.sum(np.square(input_coord[0,0,0,:]-coordinates[:,0,0,:]), -1))[:num_n]
+
+                    neighbor_coord = coordinates[indices,::]
+
+                    input_neighbors = img_data[indices,::]
+                    input_flows = precomputed_flows[indices,::]
+
+                    time_index = indices // (dims[0]*dims[1])
+                    rest = indices % (dims[0]*dims[1])
+                    view_index = rest % dims[1]
+                    albedo_index = [0]
+
+                    img = model(
+                        torch.from_numpy(input_coord).cuda().float().permute(0,3,1,2), 
+                        neighbors=torch.from_numpy(input_neighbors).cuda().float().permute(0,3,1,2),
+                        albedo_index=torch.from_numpy(albedo_index).cuda().long(),
+                        coord_neighbor=torch.from_numpy(neighbor_coord).cuda().permute(0,3,1,2), 
+                        neighbors_flow=torch.from_numpy(input_flows).cuda(), 
+                        test=True
+                    )
+
+                    img = img.cpu().detach()[0,::].permute(1,2,0).numpy()
+                    img = np.minimum(np.maximum(img, 0.0), 1.0)
+                    out.write(np.uint8(img * 255))
+                    print('\r<Gen Video> interpolated image {} of {}'.format(i+1, len(idx)), end=' ')
+
+                out.release()
+                print('')
+
 
 gen_video()
--- load_data.py
+++ load_data.py
@@ -10,7 +10,6 @@ def load_data(args):
     img_h, img_w, _ = img.shape
     img_h = img_h // args.DSfactor
     img_w = img_w // args.DSfactor
-    neighbor_num = args.neighbor_num # unused when dims = 3
     dims = args.dims
 
     print('<Load Data> Resolution after Downsampling: %d x %d' % (img_h, img_w))
@@ -20,51 +19,75 @@ def load_data(args):
     img_data = []
 
     # Only handles args.type == ['light', 'view', 'time']
-    for i in range(len(image_filenames)):
+    if(args.type == ['light', 'view', 'time']):
+        for i in range(len(image_filenames)):
 
-        print('\r<Load Data> Loading image No.{}'.format(i + 1), end=' ')
+            print('\r<Load Data> Loading image No.{}'.format(i + 1), end=' ')
+            
+            img = cv2.imread(os.path.join(args.dataset, image_filenames[i]))
+            # when downsampling images, recommend using INTER_AREA as interpolation
+            img = cv2.resize(img, dsize=(img_w, img_h), interpolation=cv2.INTER_AREA)
+            
+            img = np.float32(img) / 255.0   
+
+            time  = i // (dims[0]*dims[1])
+            rest  = i %  (dims[0]*dims[1])
+            view  = rest % dims[1]
+            light = rest // dims[1]
+
+            coordinates.append(np.array([[[light,view,time]]]))
+            img_data.append(img)
+
+            pair = np.array([light,light-1,light+1,view,view,view,time,time,time])
+            pair = np.where(pair < 0, dims[0]-1, pair)
+            pair = np.where(pair > dims[0]-1 , 0, pair)
+            pairs.append(pair)
+            
+            
+            pair = np.array([light,light,light,view,view-1,view+1,time,time,time])
+            pair = np.where(pair < 0, dims[1]-1, pair)
+            pair = np.where(pair > dims[1]-1 , 0, pair)
+            pairs.append(pair)
+            
+            
+            pair = np.array([light,light,light,view,view,view,time,time-1,time+1])
+            pair = np.where(pair < 0, dims[2]-1, pair)
+            pair = np.where(pair > dims[2]-1 , 0, pair)
+            pairs.append(pair)
         
-        img = cv2.imread(os.path.join(args.dataset, image_filenames[i]))
-        # when downsampling images, recommend using INTER_AREA as interpolation
-        img = cv2.resize(img, dsize=(img_w, img_h), interpolation=cv2.INTER_AREA)
-        
-        img = np.float32(img) / 255.0   
+        pairs = np.stack(pairs,0)
+        img_index    = pairs[:,0:3]*dims[1] + pairs[:,3:6] + pairs[:,6:9]*dims[0]*dims[1] 
+        albedo_index = pairs[:,3:4]*dims[1] + pairs[:,6:7]
+        training_pairs = np.concatenate((img_index,albedo_index),-1)
+        img_data = np.stack(img_data,0)
+        coordinates = np.stack(coordinates,0)
 
-        time  = i // (dims[0]*dims[1])
-        rest  = i %  (dims[0]*dims[1])
-        view  = rest % dims[1]
-        light = rest // dims[1]
-
-        coordinates.append(np.array([[[light,view,time]]]))
-        img_data.append(img)
-
-        pair = np.array([light,light-1,light+1,view,view,view,time,time,time])
-        pair = np.where(pair < 0, dims[0]-1, pair)
-        pair = np.where(pair > dims[0]-1 , 0, pair)
-        pairs.append(pair)
-           
-           
-        pair = np.array([light,light,light,view,view-1,view+1,time,time,time])
-        pair = np.where(pair < 0, dims[1]-1, pair)
-        pair = np.where(pair > dims[1]-1 , 0, pair)
-        pairs.append(pair)
-           
-           
-        pair = np.array([light,light,light,view,view,view,time,time-1,time+1])
-        pair = np.where(pair < 0, dims[2]-1, pair)
-        pair = np.where(pair > dims[2]-1 , 0, pair)
-        pairs.append(pair)
+        # img_data -> (img_count, h, w, RGB)
+        # coordinates -> (img_count, 1, 1, coords), coords as in [light, view, time]
+        # training_pairs -> (img_count * dims, img_indexes), img_indexes as in [self, left_neighbor, right_neighbor, albedo]
     
-    pairs = np.stack(pairs,0)
-    img_index    = pairs[:,0:3]*dims[1] + pairs[:,3:6] + pairs[:,6:9]*dims[0]*dims[1] 
-    albedo_index = pairs[:,3:4]*dims[1] + pairs[:,6:7]
-    training_pairs = np.concatenate((img_index,albedo_index),-1)
-    img_data = np.stack(img_data,0)
-    coordinates = np.stack(coordinates,0)
+    else:
+        for i in range(len(image_filenames)):
+            print('\r<Load Data> Loading image No.{}'.format(i + 1), end=' ')
+            
+            img = cv2.imread(os.path.join(args.dataset, image_filenames[i]))
+            img = cv2.resize(img, dsize=(img_w, img_h), interpolation=cv2.INTER_AREA)
+            
+            img = np.float32(img) / 255.0   
 
-    # img_data -> (img_count, h, w, RGB)
-    # coordinates -> (img_count, 1, 1, coords), coords as in [light, view, time]
-    # training_pairs -> (img_count * dims, img_indexes), img_indexes as in [self, left_neighbor, right_neighbor, albedo]
+            coordinates.append(np.array([[[i]]]))
+            img_data.append(img)
+
+            pair = np.array([i,i-1,i+1])
+            pair = np.where(pair < 0, dims[0]-1, pair)
+            pair = np.where(pair > dims[0]-1 , 0, pair)
+            pairs.append(pair)
+
+        img_data = np.stack(img_data,0)
+        coordinates = np.stack(coordinates,0)
+        print(pairs)
+        training_pairs = np.stack(pairs,0)
+        
 
     print('\n<Load Data> Finish Loading data')
 
